0430ba61df856f90e1e448549b1f3f8a3abeba02
s4r14k <sarikraf@gmail.com>
2021-03-19 17:37:33 +0100

    update https

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://balamer.fr",
+    origin: "https://balamer.fr",
     methods: ["GET", "POST"],
     credentials: true
   }
