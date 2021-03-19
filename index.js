fa6511a9fc9886ff2f22bdfb02a39ffcd61c442a
s4r14k <sarikraf@gmail.com>
2021-03-19 16:34:03 +0100

    cors origin

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const http = require('http').Server(app);
 const io = require("socket.io")(http, {
   cors: {
-    origin: "http://localhost",
+    origin: "https://agile-reef-63748.herokuapp.com",
     methods: ["GET", "POST"],
     credentials: true
   }
